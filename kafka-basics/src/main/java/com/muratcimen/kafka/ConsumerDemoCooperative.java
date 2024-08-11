35922325d65944bdbd3ec01c4febd5ba8bd71e27
Murat ÇİMEN <cimnmurat@gmail.com>
2024-08-11 16:17:21 +0300

    KE-KB / Configured Kafka consumer with CooperativeStickyAssignor.

---
+++ kafka-basics/src/main/java/com/muratcimen/kafka/ConsumerDemoCooperative.java
@@ -0,0 +1,96 @@
+package com.muratcimen.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoCooperative {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoCooperative.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer");
+
+        String groupId = "my-java-application";
+        String topic = "demo_java";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to Localhost
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        //connect to Remote Server
+        //properties.setProperty("security.protocol", "SASL_SSL");
+        //properties.setProperty("sasl.jaas.config", "");
+        //properties.setProperty("sasl.mechanism", "PLAIN");
+
+        //create consumer configs
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest"); //"none/earliest/latest"
+        properties.setProperty("partition.assignment.strategy", CooperativeStickyAssignor.class.getName());
+        //properties.setProperty("group.instance.id","....");//strategy for static assignment
+
+        //create a consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        //get a referance to the main thread
+        final Thread mainThread = Thread.currentThread();
+
+        //adding the shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Detected a shutdown, let's exit by calling consumer.wakeup()...");
+                consumer.wakeup();
+
+                //join the main thread to allow execution of the code in the main thread
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            //subscribe to a topic
+            consumer.subscribe(Arrays.asList(topic)); // consumer.subscribe(Arrays.asList(topic,topic1,topic2, ...));
+            //poll for data
+            int i = 0;
+            while (true) {
+                log.info("Poolling");
+
+                ConsumerRecords<String, String> records =
+                        consumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("Key: " + record.key() + ", Value: " + record.value());
+                    log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                }
+                // i++;
+//                if(i == 10) {
+//                    System.exit(0);
+//                }
+            }
+        } catch (WakeupException e) {
+            log.info("Consumer is starting to shut down");
+        } catch (Exception e) {
+            log.error("Unexpected exception in the consumer", e);
+        } finally {
+            consumer.close(); //close the consumer, this will also commit offsets
+            log.info("The consumer is now gracefully shut down");
+        }
+    }
+}
+
